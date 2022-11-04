31301a2728a51db30338590894878be78c41fcc7
Stephen Banbury <sbanbury@gmail.com>
2022-11-04 09:29:23 +0000

    Twenty first tranch

---
+++ Assets/Scripts/SpatialAnchorsManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class SpatialAnchorsManager : MonoBehaviour
+{
+    public static SpatialAnchorsManager instance;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
